5f2d4cce64844866cf83c53d1557fbf85909e1cd
sohailkhanosu <khanso@oregonstate.edu>
2018-03-15 20:21:19 -0700

    add json file persistence

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ const path = require('path');
 module.exports = {
     botDir: process.env.BOT_DIR || path.join(__dirname, 'bot-engines'),
     pythonPath: 'python3',
-    heartBeatInterval: 5000
+    heartBeatInterval: 5000,
+    dumpFile: 'state-dump.json',
+    dumpInterval: 60 * 1000,     /* in milliseconds */
+    useCloudStorage: false        /* enable fetch/store from cloud storage */
 }
--- store.js
+++ store.js
@@ -1,5 +1,71 @@
-class ObjectStore {}
+const fs = require('fs');
+const path = require('path');
+const config = require('./config');
+const {promisify} = require('util');
+const Storage = require('@google-cloud/storage');
+const storage = new Storage({projectId: 'camelopardalis-467-1'});
+const bucketName = 'camelopardalis-467-1-dumps'
 
-var store = new ObjectStore();
+
+function uploadFileToGCS(filename) {
+    return storage.bucket(bucketName)
+        .upload(filename)
+        .catch(err => console.log(`GCS ERROR: unable to upload ${filename}`));
+}
+
+function downloadFileFromGCS(filename, destination) {
+    let options = {destination};
+
+    return storage.bucket(bucketName)
+        .file(filename)
+        .download(options)
+        .catch(err => console.log('GCS ERROR: ', err.message || err));
+}
+
+
+function getObjectToDump(store) {
+    let dataToDump = {};
+    Object.keys(store).forEach(key=> {
+        if (key[0] === '_')
+            return;
+        dataToDump[key] = store[key];
+    });
+    return dataToDump;
+}
+
+
+class ObjectStore {
+    constructor() {
+
+        /* try loading from google cloud storage */
+        let startPromise;
+        if (config.useCloudStorage) {
+            startPromise = downloadFileFromGCS(config.dumpFile, path.join(__dirname, config.dumpFile));
+        } else {
+            startPromise = Promise.resolve();
+        }
+
+        /* load the data file if it exists, else just return */
+        startPromise.then(() => promisify(fs.readFile)(config.dumpFile, 'utf-8'))
+            .then(contents=> Object.assign(this, JSON.parse(contents)))
+            .catch(err => console.log('no dump file found, it will be created now'))
+            .then(() => {
+                /* then setup regular file backups at specified interval */
+                this._dumpLoop = setInterval(() => {
+                    let objectToDump = getObjectToDump(this);
+                    promisify(fs.writeFile)(config.dumpFile, JSON.stringify(objectToDump), 'utf-8')
+                        .catch(err => 'unable to write to ' + config.dumpFile)
+                        .then(() => {
+                            /* try to save to google cloud */
+                            if (config.useCloudStorage) {
+                                uploadFileToGCS(config.dumpFile);
+                            }
+                        });
+                }, config.dumpInterval);
+            });
+    }
+}
+
+let store = new ObjectStore();
 
 module.exports = store;
